62f32d8f23411a12e9d68c1c2fe0d11ebf907f64
Lucy Jones <lucyjpjones@gmail.com>
2021-02-28 10:00:10 +0000

    card image path updated

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -12,14 +12,14 @@ function ProjectCards() {
           <div className='cards__wrapper'>
             <ul className='cards__items'>
               <CardItem
-                src='images/resume.png'
+                src={process.env.PUBLIC_URL + '/images/resume.png'}
                 text='
                 A resume site built from pure HTML and CSS'
                 label='HTML & CSS'
                 path='https://github.com/Lucyjpjones/LucyJones-resume'
               />
               <CardItem
-                src='images/BRITbrAIN.png'
+                src={process.env.PUBLIC_URL + '/images/BRITbrAIN.png'}
                 text='An interactive map-based quiz game built mainly with Javascript'
                 label='HTML, CSS & JS'
                 path='https://github.com/Lucyjpjones/LucyJones-BRITbrAIN'
@@ -27,13 +27,13 @@ function ProjectCards() {
             </ul>
             <ul className='cards__items'>
               <CardItem
-                src='images/WeanCuisine.png'
+                src={process.env.PUBLIC_URL + '/images/WeanCuisine.png'}
                 text='A recipe site that allows users to connect, explore, and share their weaning recipes.'
                 label='Python & Flask'
                 path='https://github.com/Lucyjpjones/wean-cuisine'
               />
               <CardItem
-                src='images/fscr.png'
+                src={process.env.PUBLIC_URL + '/images/fscr.png'}
                 text='FSCR focuses on providing structured and detailed fitness programmes for football-related injuries.'
                 label='Python & Django'
                 path='https://github.com/Lucyjpjones/lj-fscr'
