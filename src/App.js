8887010178f56cff2665a711345bec5d57dddd02
Lucy Jones <lucyjpjones@gmail.com>
2021-02-28 20:53:49 +0000

    ios vh code resolution added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -8,13 +8,11 @@ import About from './components/pages/About';
 import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 import HeroSection from './components/HeroSection';
-// import useVH from 'react-viewport-height';
 
 function App() {
-  // const vh = useVH();
 
   return (
-    <div className="vh">
+    <div>
       <Router basename={process.env.PUBLIC_URL}>
         <ScrollToTop>
         <Navbar />
@@ -31,4 +29,37 @@ function App() {
   );
 }
 
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
+}
+
 export default App;
+
+
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,26 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import ITyped from 'react-ityped';
 import { Button } from './Button';
 import '../App.css'
 import './HeroSection.css'
 
-// function HeroSection() {
-//     const [text, setText] = useState("")
-//     const [fullText, setFullText] = useState("Full Stack Web Developer")
-//     const [index, setIndex] = useState(0)
-
-//     useEffect(() => {
-//         if (index < fullText.length) {
-//             setTimeout(() => {
-//                 setText(text + fullText[index])
-//                 setIndex(index + 1)
-//             }, 180)
-//         }
-//     }, [index])
-
 const HeroSection = () => {
 
-    const textStrings = ['Front End Developer', 'Back End Developer', 'Full Stack Developer', 'Based in London']
+    const textStrings = ['Front End Developer', 'Back End Developer', 'Full Stack Developer']
 
     return (
         <div id='homepage' className="hero-container">
@@ -40,17 +26,5 @@ const HeroSection = () => {
         </div>
     );
 }
-
-//     return (
-//         <div className="hero-container">
-//             <h1>
-//                 <span>I am</span>
-//                 <span>Lucy Jones</span>
-//             </h1>
-//             <p>{text}</p>
-//             <a target="_blank" rel="noreferrer" href='files/Resume-LucyJPJones.pdf' className='btn btn--primary'>Get CV</a>
-//         </div>
-//     )
-// }
 
 export default HeroSection
