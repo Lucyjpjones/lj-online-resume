f88a60365bc50e1b658253421205210fc9c52cf3
Lucy Jones <lucyjpjones@gmail.com>
2021-03-03 09:28:34 +0000

    reverted back to previous code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history'
 import './App.css';
 import ScrollToTop from './components/ScrollToTop';
 import Home from './components/pages/Home';
@@ -10,18 +9,6 @@ import Projects from './components/pages/Projects';
 import Contact from './components/pages/Contact';
 
 function App() {
-
-  const express = require('express');
-  const path = require('path');
-  const app = express();
-
-  app.use(express.static(path.join(__dirname, 'build')));
-
-  app.get('/lj-online-resume/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-
-  app.listen(9000);
 
   return (
     <>
