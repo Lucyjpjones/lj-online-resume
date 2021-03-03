374e1716852e94922bc49b04f0aa87f0071206f0
Lucy Jones <lucyjpjones@gmail.com>
2021-03-03 10:54:53 +0000

    added 404 page

---
+++ src/components/NotFoundSection.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../App.css'
+import { Link } from 'react-router-dom'
+import './HeroSection.css'
+
+function NotFoundSection(props) {
+
+    return (
+        <div className="hero-container">
+            <h1>
+                <span className="underline--text" style={{'margin':'auto', 'width':'fit-content'}}>404</span>
+                <span>Page not found</span>
+            </h1>
+            <div>
+                <Link to='/' className='nav-links btn-cv'>
+                    Home
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default NotFoundSection
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../App.css'
+import NotFoundSection from '../NotFoundSection'
+
+function NotFound() {
+    return (
+        <>
+            <NotFoundSection />
+        </>
+    )
+}
+
+export default NotFound
