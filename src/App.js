8755fa80d83d81e388abd57aea5a9f94903dd191
Lucy Jones <lucyjpjones@gmail.com>
2021-03-03 09:23:52 +0000

    changed path in get method

--- src/App.js
+++ src/App.js
@@ -16,8 +16,8 @@ function App() {
   const app = express();
 
   app.use(express.static(path.join(__dirname, 'build')));
-  
-  app.get('/*', function (req, res) {
+
+  app.get('/lj-online-resume/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
