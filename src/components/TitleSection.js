c9ae0f74b2946218c35e2a30dc75533468d3e982
Lucy Jones <lucyjpjones@gmail.com>
2021-03-09 12:41:16 +0000

    updated down arrow

--- src/components/TitleSection.js
+++ src/components/TitleSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BsChevronDoubleDown } from "react-icons/bs";
 import '../App.css'
 import './HeroSection.css'
 
@@ -10,7 +11,7 @@ function TitleSection(props) {
                 <span className="underline--text">{props.title}</span>
             </h1>
             <a href={props.navlink} className='arrow'>
-                <i className="fas fa-angle-double-down fa-3x bounce" style={{marginTop:'100px', color:'#B7CCB7'}}></i>
+                <BsChevronDoubleDown className="fa-3x bounce" style={{marginTop:'100px', color:'#B7CCB7'}}/>
             </a>
         </div>
     )
